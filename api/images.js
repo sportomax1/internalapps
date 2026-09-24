ba76d0560f4a170b13f220647452d7909bbba38b
sportomax1 <avalancheofnuggets@gmail.com>
2026-09-24 07:28:24 -0600

    Force Image Finder API to Node runtime

--- api/images.js
+++ api/images.js
@@ -1,4 +1,4 @@
-const UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36';
+export const config = { runtime: 'nodejs' };\nconst UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36';
 
 function tokenFrom(text){
   const patterns=[
