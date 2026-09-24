1f97c84c6f94d21af82d43c93103ef44ee0b4ce6
sportomax1 <avalancheofnuggets@gmail.com>
2026-09-24 07:33:27 -0600

    Remove standalone API now handled by shared tools function

--- api/env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  // Safe endpoint: does NOT return secret values, only presence flags
-  const hasAppPassword = !!process.env.APP_PASSWORD;
-  const hasOuraKey = !!process.env.OURA_KEY;
-
-  res.status(200).json({ appPassword: hasAppPassword, ouraKey: hasOuraKey });
-}
